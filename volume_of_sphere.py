97ab55955391f86c12c7bf09a224f6f20125e258
Hongjing <j0i5n1g3@gmail.com>
2023-09-14 22:13:04 -0500

    <Change print statement>

--- volume_of_sphere.py
+++ volume_of_sphere.py
@@ -1,8 +1,13 @@
 import math
 
 def calculate_volume_of_sphere(radius):
-  v = (3/4) * math.pi * (radius ** 3)
+  v = (4/3) * math.pi * (radius ** 3)
   return v
 
-calculate_volume_of_sphere(30)
-calculate_volume_of_sphere(40)
+radius1 = 30
+v1 = calculate_volume_of_sphere(radius1)
+print(f"The volume of sphere with radius {radius1} is: {v1}")
+
+radius2 = 40
+v2 = calculate_volume_of_sphere(radius2)
+print(f"The volume of sphere with radius {radius1} is: {v2}")
